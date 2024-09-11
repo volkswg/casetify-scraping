3835c5ce298a730c14ffb4130537277032ff580e
Saranpat <saranpat.fu@easysunday.com>
2024-09-11 21:55:47 +0700

    optimize calculate price

--- module/Math.py
+++ module/Math.py
@@ -5,16 +5,9 @@ def roundUpHundred(x):
   return int(math.ceil(x / 100.0)) * 100  
 
 def calculateSellingPrice(price, preorder=False, colab=False):
-  sellingPrice = price
   try:
-    sellingPrice = GlobalVar.databaseInstance.getSellingPrice(price, colab)
+    return GlobalVar.databaseInstance.getSellingPrice(price, colab)
   except:
-    multipyFactor = 1.31
-    additionalValue = 0
-    if preorder == False:
-      additionalValue = 300
-    if colab == True:
-      multipyFactor = 1.50
-    sellingPrice = roundUpHundred((price + additionalValue) * multipyFactor) - 1
-  # Logger.logDebug(f"price = {price} => {sellingPrice}")
-  return sellingPrice
+    multiplyFactor = 1.50 if colab else 1.31
+    additionalValue = 0 if preorder else 300
+    return roundUpHundred((price + additionalValue) * multiplyFactor) - 1
